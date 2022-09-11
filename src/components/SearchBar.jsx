2e32ace5b1eb68b1e4eb71081b34cb9a05cff688
Marksolidor <marco350sl@gmail.com>
2022-09-11 20:53:57 -0300

    feat printing data from api

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,32 +1,36 @@
 import React from "react";
-import {searchPokemon} from '../api';
-const {useState} = React;
-
+import { searchPokemon } from "../api";
+const { useState } = React;
 
 const SearchBar = () => {
-    const [search, setSearch] = useState('');
-    
-    const searching = (e) => {
-        console.log(e.target.value);
-        setSearch (e.target.value);
-    }
-    const findPoke = async (e) => {
-        const data = await searchPokemon(search);
-        console.log(data);
-    };
-    
-    return (
-<>
-<div>
-    <input  
-    placeholder="Buscar pokemon"
-    onChange={searching}/>
-</div>
-<div>
-    <button onClick={findPoke}> Buscar </button>
-</div>
-</>
-    );
-}
+  const [search, setSearch] = useState("");
+  const [pokemon, setPokemon] = useState("");
+  const searching = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+  };
+  const findPoke = async (e) => {
+    const data = await searchPokemon(search);
+    setPokemon(data);
+  };
 
-export default SearchBar
+  return (
+    <>
+      <div>
+        <input placeholder="Buscar pokemon" onChange={searching} />
+      </div>
+      <div>
+        <button onClick={findPoke}> Buscar </button>
+      </div>
+      <div>
+        {pokemon && (
+          <div>
+            <div>{pokemon.name}</div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default SearchBar;
