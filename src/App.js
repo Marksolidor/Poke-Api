ca39ac62eff08f900ccbdbced6e8a36886cf565d
Marksolidor <marco350sl@gmail.com>
2022-09-11 23:42:10 -0300

    feat getting 10 objets from the api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Pokedex from "./components/Pokedex";
 import SearchBar from "./components/SearchBar";
+import {getPokemons} from "./api";
+
+const {useState, useEffect} = React;
 
 export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async() => {
+    try{
+      const data = await getPokemons();
+      console.log(data)
+    } catch(err){
+
+    }
+  }
+
+  useEffect(() => {
+  fetchPokemons();
+  }, [])
   return (
     <>
     <div>
@@ -10,6 +28,7 @@ export default function App() {
     </div>
     <div>
       <SearchBar/>
+      <Pokedex/>
     </div>
 
     </>
---
+++ src/components/Pokedex.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pokedex = () =>{
+    return(
+        <div>
+            <div>
+            <h1>Pokedex</h1>
+            <div>Pagination</div>
+            </div>
+        </div>
+    );
+};
+export default Pokedex
