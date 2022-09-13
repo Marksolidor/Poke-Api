8d242a4610f5b104d7a8e38073c3169ce208fc37
Marksolidor <marco350sl@gmail.com>
2022-09-13 20:49:00 -0300

    feat pagination

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,31 +1,42 @@
 import React from "react";
+import Pagination from "./Pagination";
 import Pokemon from "./Pokemon";
-import Pagination from "./Pagination"
 
-const Pokedex = (props) =>{
-    console.log(props)
-    const {pokemons} = props;
-    return(
+const Pokedex = (props) => {
+  const { pokemons, page, setPage, total, loading } = props;
+
+  const lastPage = () => {
+    const nextPage = Math.max(page - 1, 0);
+    setPage(nextPage);
+  };
+
+  const nextPage = () => {
+    const nextPage = Math.min(page + 1, total - 1);
+    setPage(nextPage);
+  };
+
+  return (
+    <div>
+      <div className="header">
+        <h1>Pokedex</h1>
+        <Pagination
+          page={page + 1}
+          totalPages={total}
+          onLeftClick={lastPage}
+          onRightClick={nextPage}
+        />
+      </div>
+      {loading ? (
+        <div>Cargando pokemons...</div>
+      ) : (
         <div>
-            <div>
-            <h1>Pokedex</h1>
-            <Pagination
-            page={1}
-            totalPages={100}
-            onLeftClick={console.log}
-            onRIghtClick={console.log}
-            />
-            </div>
-            <div>
-                {pokemons.map((pokemon, idx) =>{
-                    return(
-                        <Pokemon pokemon={pokemon} key={pokemon.name}/>
-                    )
-                }
-                
-                )}
-            </div>
+          {pokemons.map((pokemon, idx) => {
+            return <Pokemon pokemon={pokemon} key={pokemon.name} />;
+          })}
         </div>
-    );
+      )}
+    </div>
+  );
 };
-export default Pokedex
+
+export default Pokedex;
